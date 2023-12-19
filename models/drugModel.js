7ad13db5367a6622dcc0dddaeb36284ffbe3b7a7
user-byte123 <ewong1217@gmail.com>
2023-12-19 13:05:36 -0800

    Can serve static HTML file, but post is not working (addd urlencoded)

--- models/drugModel.js
+++ models/drugModel.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://ewong1217:8qogx2qu38W3Nasu@cluster0.vq8eez3.mongodb.net/?retryWrites=true&w=majority';
+// const MONGO_URI = "mongodb+srv://ewong1217:8qogx2qu38W3Nasu@cluster0.vq8eez3.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(MONGO_URI, {
-  // options for the connect method to parse the URI
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // sets the name of the DB that our collections are part of
-  dbName: 'Formulary'
-})
-  .then(() => console.log('Connected to Mongo DB.'))
-  .catch(err => console.log(err));
+// mongoose.connect(MONGO_URI, {
+//   // options for the connect method to parse the URI
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   // sets the name of the DB that our collections are part of
+//   dbName: 'Formulary'
+// })
+//   .then(() => console.log('Connected to Mongo DB.'))
+//   .catch(err => console.error('Error connecting to MongoDB:', err))
+
 
 const Schema = mongoose.Schema;
 
--- server.js
+++ server.js
@@ -2,14 +2,44 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/api.js')
+const mongoose = require('mongoose');
 
+const MONGO_URI = "mongodb+srv://ewong1217:8qogx2qu38W3Nasu@cluster0.vq8eez3.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI, {
+  // options for the connect method to parse the URI
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // sets the name of the DB that our collections are part of
+  dbName: 'Formulary'
+})
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.error('Error connecting to MongoDB:', err))
+
+//I think i need to be able to parse the request body:
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
+//define route handlers
+app.use('/diabetes', apiRouter);
 
-app.get('/', (req,res) => {
-    res.send('Hello, world!');
-});
+app.use(express.static(path.resolve(__dirname, './assets')));
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
     console.log(`Server is running on http://localhost:${PORT}`);
-}
+});
