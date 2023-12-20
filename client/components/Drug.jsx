37c65d1e7ecffc8a33108c36a48eb838795e9ba4
user-byte123 <ewong1217@gmail.com>
2023-12-19 18:48:00 -0800

    working on Drug.jsx

--- client/components/Drug.jsx
+++ client/components/Drug.jsx
@@ -4,5 +4,9 @@ import { faMoneyBill1 } from '@fortawesome/free-regular-svg-icons'
 
 const Drug = {{
     //open modal --> used to control visibility or state of a modal = a fxn that changes a starte variable, triggering the modal to open or close
-    
-}}
+    info, openModal
+}} => {
+    const {
+        name, strength, measurement, 
+    }
+}
